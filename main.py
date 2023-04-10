6567dc18aa3b71ec03d3750ca465e896c5c34963
AyushGupta57 <ayushgupta12122001@gmail.com>
2023-04-10 21:29:51 +0530

    Added monitor name and price scraping

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-# import the required modules
 import requests
 from bs4 import BeautifulSoup
 
-# Loop continuously
-while True:
-    # Send a GET request to the Amazon India website
-    request = requests.get("https://www.amazon.in/s?k=monitors")
+monitor_names = list()
+monitor_hrefs = list()
+monitor_price = list()
 
-    # Check if the request was successful (status code 200)
+page = 1
+
+while True:
+
+    url = "https://www.amazon.in/s?k=monitors&page=" + str(page)
+    request = requests.get(url)
+
     if request.status_code == 200:
-        # Get the HTML content of the webpage
+
         html = request.content
 
-        # Create a BeautifulSoup object with the HTML content and parse it
         soup = BeautifulSoup(html, "html.parser")
 
-        # Create an empty list to store href links
-        href_list = list()
-
-        # Loop through all links on the page with class "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"
         for a_href in soup.find_all("a", {"class": "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"}):
-            # Get the href attribute of each link and add it to the href_list
-            href_list.append(a_href.get('href'))
+            monitor_hrefs.append(a_href.get('href'))
 
-        # Print the list of href links
-        print(href_list)
+        for names in soup.find_all("span", {"class": "a-size-medium a-color-base a-text-normal"}):
+            monitor_names.append(names.text)
 
-        # Exit the loop
+        for prices in soup.find_all("span", {"class": "a-price-whole"}):
+            monitor_price.append(prices.text)
+
+        print(monitor_hrefs)
+        print(monitor_names)
+        print(monitor_price)
+
         break
